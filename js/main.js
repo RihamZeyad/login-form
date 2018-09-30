dd3b468c21d5959902ff9a956f5d3e4452ebc309
RihamZeyad <42995084+RihamZeyad@users.noreply.github.com>
2018-09-30 13:01:35 +0300

    login
    JQUERY

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	
+	$(".startBttn").click(function(){
+		$(".pop").fadeIn(600);
+	});
+		
+
+$(".submitBtn").click(function(){
+	$(".pop").fadeOut(800);
+});
+	
+	$(".shadowBack").click(function(){
+		$(".pop").fadeOut(800);
+	});
+	
+});
